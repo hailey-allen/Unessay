a01862740c80dba0456c813f083ee371046900cb
Lubdhak Juyal <22012050+monsoonssn@users.noreply.github.com>
2020-11-10 21:51:11 -0700

    Adding basic default theme and some more assets

--- src/ui/SceneChangeButton.gd
+++ src/ui/SceneChangeButton.gd
@@ -1,10 +1,8 @@
 tool
 extends Button
 
-export(String, FILE) var next_scene_path: = ""
+export(String, FILE, '*.tscn') var next_scene_path: = ""
 
-func _on_button_up() -> void:
-	get_tree().change_scene(next_scene_path)
-	
+
 func _get_configuration_warning() -> String:
 	return "The Next Level property can't be empty" if next_scene_path == "" else ""
