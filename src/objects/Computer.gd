98c8bf626860a0d3e183e46554a27e198403cdcf
monsoonssn <22012050+monsoonssn@users.noreply.github.com>
2020-12-11 13:40:05 -0700

    better syntax in Computer.gd

--- src/objects/Computer.gd
+++ src/objects/Computer.gd
@@ -8,9 +8,9 @@ func makevis():
 	$CanvasLayer/ComputerScreen.visible = !$CanvasLayer/ComputerScreen.visible
 	
 func _input(event):
-	if $CanvasLayer/ComputerScreen.visible &&  event.is_action_pressed("interact"):
+	if $CanvasLayer/ComputerScreen.visible && event.is_action_pressed("interact"):
 		pass
 
-func	set_label(Label):
-		Label.text = "Use"
+func set_label(Label):
+	Label.text = "Use"
 
