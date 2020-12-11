3d1b7bc844edf31277cc3e6a13c71acbc81631a7
monsoonssn <22012050+monsoonssn@users.noreply.github.com>
2020-12-11 16:42:58 -0700

    fully implemented password game

--- assets/ui/Password menu/ComputerScreen.gd
+++ assets/ui/Password menu/ComputerScreen.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 onready var input_box: LineEdit = $InputBox
+# The password to enter into the textbox"
+const PASSWORD = "password"
 
+signal verified
 
 func get_notes_string() -> String:
 	var notes = Inventory.get_notes()
@@ -14,3 +17,11 @@ func get_notes_string() -> String:
 
 func _on_ComputerScreen_visibility_changed() -> void:
 	$Hints.text = get_notes_string()
+
+
+func _on_InputBox_text_entered(new_text: String) -> void:
+	"This is what happens when you press enter on the input box"
+	if new_text == PASSWORD:
+		emit_signal("verified")
+	
+	input_box.clear()
--- src/objects/Computer.gd
+++ src/objects/Computer.gd
@@ -2,20 +2,34 @@ extends StaticBody2D
 # Level03 init SpawnLocation pos: 64,128
 
 onready var screen: = $CanvasLayer/ComputerScreen
+onready var tree: = get_tree()
+
+signal verified # lol 
 
 func _ready() -> void:
 	screen.visible = false
 
 func interaction() -> void:
-	makevis()
+	toggle_visibility()
 	
-func makevis():
+func toggle_visibility():
 	screen.visible = !screen.visible
 	
 func _input(event):
-	if screen.visible && event.is_action_pressed("interact"):
-		pass
+	if screen.visible && event.is_action_pressed("pause"):
+		toggle_visibility()
+
 
 func set_label(Label):
 	Label.text = "Use"
 
+
+func _on_ComputerScreen_visibility_changed() -> void:
+	if tree.paused:
+		tree.paused = false
+	else:
+		tree.paused = true
+
+
+func _on_ComputerScreen_verified() -> void:
+	emit_signal("verified")
