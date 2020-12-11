87a557b494b370fc776ce12fa6ada7467326d46d
monsoonssn <22012050+monsoonssn@users.noreply.github.com>
2020-12-11 13:02:51 -0700

    popup on note interact

--- src/objects/Note.gd
+++ src/objects/Note.gd
@@ -1,16 +1,35 @@
 extends StaticBody2D
 
 onready var anim_player: AnimationPlayer = $AnimationPlayer
+onready var popup: Popup = $CanvasLayer/Popup
 # This note variable must be different for each note
 export var note: = ""
+export var is_fragment = false # True is fragment, false is hint
+
+const FRAGMENT: String = "Fragment of the password: "
+const HINT: String = "Password hint: "
 
 func display_popup():
-	pass
+	popup.popup_centered()
+	if is_fragment:
+		popup.get_node("Label").text = FRAGMENT + note
+	else:
+		popup.get_node("Label").text = HINT + note
+	get_tree().paused = true
+	
 
 func interaction():
 	Inventory.notes.append(note)
 	print(Inventory.get_notes())
+	display_popup()
+	
+	
 	anim_player.play("fade_out")
 
 func set_label(Label):
 	Label.text = "Read"
+
+
+func _on_Close_button_up() -> void:
+	popup.hide()
+	get_tree().paused = false
